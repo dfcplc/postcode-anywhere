687a1284cb77e0e24e13d07ac007e62a1c310034
Matt Clements <matt@mattclements.co.uk>
2015-02-26 10:29:51 +0000

    Fixed Exception Handling

--- src/Dfcplc/PostcodeAnywhere/PostcodeAnywhere/Find.php
+++ src/Dfcplc/PostcodeAnywhere/PostcodeAnywhere/Find.php
@@ -43,7 +43,7 @@ class Lookup
 		//Check for an error, if there is one then throw an exception
 		if ($file->Columns->Column->attributes()->Name == "Error")
 		{
-			throw new Exception("[ID] " . $file->Rows->Row->attributes()->Error . " [DESCRIPTION] " . $file->Rows->Row->attributes()->Description . " [CAUSE] " . $file->Rows->Row->attributes()->Cause . " [RESOLUTION] " . $file->Rows->Row->attributes()->Resolution);
+			throw new \Exception("[ID] " . $file->Rows->Row->attributes()->Error . " [DESCRIPTION] " . $file->Rows->Row->attributes()->Description . " [CAUSE] " . $file->Rows->Row->attributes()->Cause . " [RESOLUTION] " . $file->Rows->Row->attributes()->Resolution);
 		}
 
 		//Copy the data
